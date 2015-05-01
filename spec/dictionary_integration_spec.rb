7e25f8855a86f2a86da62b781ef27ffc1020402d
Paul <student@epicodus.com>
2015-05-01 16:47:30 -0700

    it adds integration specs for new contact path and new definition path. All tests passing

--- spec/dictionary_integration_spec.rb
+++ spec/dictionary_integration_spec.rb
@@ -8,12 +8,35 @@ set(:show_exceptions, false)
 
 
 
-describe('the index', {:type => :feature}) do
-  it('has the header that describes what the headline is') do
-    visit('/')
-    expect(page).to have_content('Dictionary Ogawa')
+  describe('the index', {:type => :feature}) do
+    it('has the header that describes what the headline is') do
+      visit('/')
+      expect(page).to have_content('Dictionary Ogawa')
+    end
+
+  describe('add word path') do
+    it('adds a word to the dictionary') do
+      visit('/')
+      click_link('Add word')
+      fill_in('name', with: 'word')
+      click_button('Add word')
+      expect(page).to have_content('word')
+    end
   end
 
-  
+
+  describe('add description path', {:type => :feature}) do
+  it("adds a description number to the contact") do
+    visit '/'
+    click_link('word')
+    click_link('Add a definition')
+    fill_in('definition', :with => 'it is a word')
+    click_button('Submit')
+    click_link 'Return to homepage'
+    click_link('word')
+    expect(page).to have_content('it is a word')
+  end
+end
+
 
 end
